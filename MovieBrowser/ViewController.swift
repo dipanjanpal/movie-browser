91a9fbb0c357f69aff14d3037994a2c6871567a0
Dipanjan Pal <dipanjanpal@hotmail.com>
2020-04-01 19:43:11 +0530

    Initial Commit

---
+++ MovieBrowser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieBrowser
+//
+//  Created by Dipanjan Pal on 01/04/20.
+//  Copyright © 2020 Dipanjan Pal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
